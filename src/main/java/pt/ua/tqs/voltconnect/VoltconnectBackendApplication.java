a80a62b856cf0697d75d5583989f433fdd68a66c
João Varela <joaovarela@ua.pt>
2025-04-18 17:02:40 +0100

    setup Spring Boot API with complete dependencies and PostgreSQL in Docker container

---
+++ src/main/java/pt/ua/tqs/voltconnect/VoltconnectBackendApplication.java
@@ -0,0 +1,10 @@
+package pt.ua.tqs.voltconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class VoltconnectBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VoltconnectBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/pt/ua/tqs/voltconnect/backend/VoltconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.tqs.voltconnect.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
